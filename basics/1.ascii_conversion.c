92121c1bdceb5b909056f1954c1255da6b271c71
Md Ashiouzzaman <md.ashiouzzaman@asthait.com>
2023-06-25 07:52:41 +0600

    ASCII Conversion

---
+++ basics/1.ascii_conversion.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+    // We will take an integer ASCII value & show the corresponding ASCII character
+
+    printf("Enter any ASCII number: ");
+    int ascii_num;
+    scanf("%d", &ascii_num); // Taking the input
+    printf("ASCII character for %d is %c. \n", ascii_num, ascii_num); // Converting the integer into ASCII char.
+
+    
+}
