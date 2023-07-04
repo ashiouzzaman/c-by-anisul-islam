6897eefe1837398b86f7674b5cf214e83a342355
ashiouzzaman <ashiouzzaman.code@gmail.com>
2023-07-04 11:30:37 +0600

    GCD in C

---
+++ basics/37.G.C.D_L.C.M.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+    int num1, num2, n1, n2, reminder, gcd, lcm;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &num1, &num2);
+
+    // GCD - Greater common divisor
+    n1 = num1, n2 = num2; // temporary variable for calculation.
+    while(n2 != 0){
+        reminder = n1 % n2;
+        n1 = n2;
+        n2 = reminder;
+        gcd = n1;
+    }
+
+    printf("G.C.D of %d & %d is = %d\n", num1, num2, gcd);
+}
