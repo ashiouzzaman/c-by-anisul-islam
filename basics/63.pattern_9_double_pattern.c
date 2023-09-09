bc12f7b9a08d539ce9b5615bacee0e3f7d05b40f
ashiouzzaman <ashiouzzaman.code@gmail.com>
2023-09-09 17:31:04 +0600

    Double pattern reverse - 2nd method

--- basics/63.pattern_9_double_pattern.c
+++ basics/63.pattern_9_double_pattern.c
@@ -16,36 +16,68 @@ int main(){
     printf("Enter N: ");
     scanf("%d", &n);
 
-    // Pattern logic
-    for (int row = 1; row <= n; row++){
-        if(row < n){
-            int count = 1;
-            for(int col = n; col>= 1; col--){
-                if (col>row)
-                {
-                    printf("  ");
-                }else{
-                    printf("%d ", count);
-                    count++;
-                }
-                
-            }
-        }
-        else if(row == n){
-            for(int temp_row = n; temp_row >= 1; temp_row--){
-                int count = 1;
-                for(int col = n; col >= 1; col--){
-                    if(col > temp_row){
-                        printf("  ");
-                    }else{
-                        printf("%d ", count);
-                        count++;
-                    }
-                }
-                printf("\n");
-            }
-        }
-        printf("\n");
 
+    ////////////////////////////
+    ///////// 1st method //////
+    //////////////////////////
+
+    // // Pattern logic
+    // for (int row = 1; row <= n; row++){
+    //     if(row < n){
+    //         int count = 1;
+    //         for(int col = n; col>= 1; col--){
+    //             if (col>row)
+    //             {
+    //                 printf("  ");
+    //             }else{
+    //                 printf("%d ", count);
+    //                 count++;
+    //             }
+                
+    //         }
+    //     }
+    //     else if(row == n){
+    //         for(int temp_row = n; temp_row >= 1; temp_row--){
+    //             int count = 1;
+    //             for(int col = n; col >= 1; col--){
+    //                 if(col > temp_row){
+    //                     printf("  ");
+    //                 }else{
+    //                     printf("%d ", count);
+    //                     count++;
+    //                 }
+    //             }
+    //             printf("\n");
+    //         }
+    //     }
+    //     printf("\n");
+
+    // }
+
+
+    ////////////////////////////
+    ///////// 2nd method //////
+    //////////////////////////
+
+    for (int row = 1; row <= n; row++){
+        for(int col = 1; col <= n - row; col++){
+            printf("  ");
+        }
+        for(int col = 1; col <= row; col++){
+            printf("%d ", col);
+        }
+
+        printf("\n");
     }
+    
+    for (int row = n-1; row >= 1; row--){
+            for(int col = 1; col <= n - row; col++){
+                printf("  ");
+            }
+            for(int col = 1; col <= row; col++){
+                printf("%d ", col);
+            }
+            printf("\n");
+        }
+
 }
