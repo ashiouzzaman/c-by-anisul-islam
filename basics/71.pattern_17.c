961c1f82142e3a846fd3827b2839cd254b45399b
ashiouzzaman <ashiouzzaman.code@gmail.com>
2023-10-29 21:51:51 +0600

    optimize if condition for printing *

--- basics/71.pattern_17.c
+++ basics/71.pattern_17.c
@@ -26,7 +26,7 @@ int main(){
     // Pattern logic
     for(row = 1; row <= n; row++){
         for(col = 1; col <= row; col++){
-            if(row == 1 || row == n || col == 1 || col == row){
+            if(row == n || col == 1 || col == row){
                 printf("* ");
             }else{
                 printf("  ");
